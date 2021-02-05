b194e5c9d796201c2991be444cfb7781670af89a
mazqt <jesperhipersson@gmail.com>
2021-02-05 20:20:26 +0100

    Implemented the pawn legal captures

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,10 +1,11 @@
 class Pawn
   include Piece
-  attr_reader :legal_moves
+  attr_reader :legal_moves, :legal_captures
 
    def initialize(colour)
     @colour = colour
     @legal_moves = set_legal_moves
+    @legal_captures = set_legal_captures
   end
 
   def set_legal_moves
@@ -21,6 +22,26 @@ class Pawn
       else
         moves << [(y - 1), x] if y > 0
         moves << [(y - 2), x] if y == 6
+      end
+      legal_moves[position] = moves
+    end
+    legal_moves
+  end
+
+  def set_legal_captures
+    mock_board = create_board()
+    legal_moves = {}
+
+    mock_board.each do |position|
+      moves = []
+      y = position[0]
+      x = position[1]
+      if @colour == "white"
+        moves << [(y + 1), (x + 1)] if y < 7
+        moves << [(y + 1), (x - 1)] if y < 7
+      else
+        moves << [(y - 1), (x + 1)] if y > 0
+        moves << [(y - 1), (x - 1)] if y > 0
       end
       legal_moves[position] = moves
     end
