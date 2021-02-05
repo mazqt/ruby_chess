787225fb2806357628a682a1a19209f8120be06b
mazqt <jesperhipersson@gmail.com>
2021-02-05 19:56:44 +0100

    X and Y axis were named incorrectly in function.

--- lib/king.rb
+++ lib/king.rb
@@ -13,24 +13,24 @@ class King
 
     mock_board.each do |position|
       moves = []
-      x_axis = position[0]
-      x_plus = x_axis + 1
-      x_minus = x_axis - 1
-      y_axis = position[1]
+      y_axis = position[0]
       y_plus = y_axis + 1
       y_minus = y_axis - 1
+      x_axis = position[1]
+      x_plus = x_axis + 1
+      x_minus = x_axis - 1
 
-      moves << [x_axis, y_minus] if y_minus >= 0
-      moves << [x_axis, y_plus] if y_plus <= 7
-      if x_plus <= 7
-        moves << [x_plus, y_axis]
-        moves << [x_plus, y_minus] if y_minus >= 0
-        moves << [x_plus, y_plus] if y_plus <= 7
+      moves << [y_axis, x_minus] if x_minus >= 0
+      moves << [y_axis, x_plus] if x_plus <= 7
+      if y_plus <= 7
+        moves << [y_plus, x_axis]
+        moves << [y_plus, x_minus] if x_minus >= 0
+        moves << [y_plus, x_plus] if x_plus <= 7
       end
-      if x_minus >= 0
-        moves << [x_minus, y_axis]
-        moves << [x_minus, y_minus] if y_minus >= 0
-        moves << [x_minus, y_plus] if y_plus <= 7
+      if y_minus >= 0
+        moves << [y_minus, x_axis]
+        moves << [y_minus, x_minus] if x_minus >= 0
+        moves << [y_minus, x_plus] if x_plus <= 7
       end
 
       legal_moves[position] = moves
