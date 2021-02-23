3054ce4825af20634e687f57d75f52209e4bdb1b
mazqt <jesperhipersson@gmail.com>
2021-02-23 01:25:26 +0100

    Made sure to change how the board is copied and made the checkmate function work. Should probably still change it so that it takes into account differentiating between player checks, otherwise it will call checkmate if I exit my check by putting my opponent in check

--- lib/board.rb
+++ lib/board.rb
@@ -2,9 +2,14 @@ require 'byebug'
 class Board
   attr_reader :board, :captured_pieces
 
-  def initialize()
-    @board = Array.new(8) { Array.new(8, "_") }
-    populate_board()
+  def initialize(board = nil)
+    if board == nil
+      @board = Array.new(8) { Array.new(8, "_") }
+      populate_board()
+    else
+      @board = Array.new(8) { Array.new(8, "_") }
+      copy_board(board)
+    end
     @captured_pieces = []
   end
 
@@ -31,6 +36,22 @@ class Board
       @board[7][index] = Bishop.new("black") if index == 2 || index == 5
       @board[7][index] = Queen.new("black") if index == 3
       @board[7][index] = King.new("black") if index == 4
+    end
+  end
+
+  def copy_board(board)
+    board.each_with_index do |row, y|
+      row.each_with_index do |piece, x|
+        if piece != "_"
+          colour = piece.colour
+          @board[y][x] = Pawn.new(colour) if piece.is_a?(Pawn)
+          @board[y][x] = Rook.new(colour) if piece.is_a?(Rook)
+          @board[y][x] = Knight.new(colour) if piece.is_a?(Knight)
+          @board[y][x] = Bishop.new(colour) if piece.is_a?(Bishop)
+          @board[y][x] = Queen.new(colour) if piece.is_a?(Queen)
+          @board[y][x] = King.new(colour) if piece.is_a?(King)
+        end
+      end
     end
   end
 
@@ -101,6 +122,13 @@ class Board
   end
 
   def clear_path?(from, to)
+
+    if @board[to[0]][to[1]] != "_" && @board[from[0]][from[1]] != "_"
+      return false if @board[from[0]][from[1]].colour == @board[to[0]][to[1]].colour
+    end
+
+    return true if @board[from[0]][from[1]].is_a?(Knight)
+
     if from[1] == to[1]
       horizontal = from[1]
       ((from[0] + 1)...to[0]).each { |vertical| return false if @board[vertical][horizontal] != "_"} if to[0] > from[0]
@@ -147,6 +175,29 @@ class Board
       end
     end
     false
+  end
+
+  def checkmate(colour)
+    king_pos = find_king(colour)
+
+    #debugger
+    @board.each_with_index do |row, y|
+      row.each_with_index do |piece, x|
+        if piece != "_"
+          if piece.colour == colour
+            legal_moves = piece.legal_moves[[y, x]]
+            legal_moves.each do |move|
+              if clear_path?([y, x], move)
+                hypothetical_board = Board.new(@board)
+                hypothetical_board.move_piece([y, x], move)
+                return false if !hypothetical_board.check
+              end
+            end
+          end
+        end
+      end
+    end
+    true
   end
 
   def find_king(colour)
