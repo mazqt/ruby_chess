e242bb417fa5ce5329ebf243d5e48e35b045fa06
mazqt <jesperhipersson@gmail.com>
2021-03-01 21:42:10 +0100

    Adjusted the specs to account for some changes to the board implementation

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -15,58 +15,54 @@ describe Board do
   end
 
   describe "#validate_input" do
-    subject(:validate_board) { described_class.new }
-
     context "when fed input that is too long" do
       it "returns false" do
-        result = validate_board.validate_input("fgdga")
+        result = Board.validate_input("fgdga")
         expect(result).to eq(false)
       end
     end
 
     context "when fed input with the wrong letter" do
       it "returns false" do
-        result = validate_board.validate_input("p3")
+        result = Board.validate_input("p3")
         expect(result).to eq(false)
       end
     end
 
     context "when fed input with the wrong number" do
       it "returns false" do
-        result = validate_board.validate_input("h9")
+        result = Board.validate_input("h9")
         expect(result).to eq(false)
       end
     end
 
     context "when fed input that is reversed" do
       it "returns false" do
-        result = validate_board.validate_input("4d")
+        result = Board.validate_input("4d")
         expect(result).to eq(false)
       end
     end
 
     context "when fed valid input" do
       it "returns true" do
-        result = validate_board.validate_input("c4")
+        result = Board.validate_input("c4")
         expect(result).to eq(true)
       end
     end
   end
 
   describe "#convert_input" do
-    subject(:input_board) { described_class.new }
-
     context "when fed algebraic notation ie c6" do
 
       it "converts it into the appropriate board position" do
-        result = input_board.convert_input("c1")
+        result = Board.convert_input("c1")
         expect(result).to eq([0,2])
       end
     end
     context "when fed faulty notation ie l3 or c9" do
       it "returns false" do
-        wrong_num = input_board.convert_input("c9")
-        wrong_let = input_board.convert_input("l3")
+        wrong_num = Board.convert_input("c9")
+        wrong_let = Board.convert_input("l3")
         expect(wrong_let).to eq(false)
         expect(wrong_num).to eq(false)
       end
@@ -246,8 +242,10 @@ describe Board do
     context "when every possible move leads to check" do
       subject(:mated_board) { described_class.new }
       before do
-        mated_board.move_piece([0, 4], [1, 4])
-        mated_board.move_piece([7, 3], [3, 4])
+        mated_board.move_piece([1, 5], [2, 5])
+        mated_board.move_piece([1, 6], [3, 6])
+        mated_board.move_piece([6, 4], [4, 4])
+        mated_board.move_piece([7, 3], [3, 7])
       end
       it "it returns true" do
         result = mated_board.checkmate("white")
