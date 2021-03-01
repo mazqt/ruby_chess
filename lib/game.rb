da81413237294803883aa2c52ed331c1a362f504
mazqt <jesperhipersson@gmail.com>
2021-03-01 19:16:11 +0100

    Added functions to serialize and save a game

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
+require "yaml"
 class Game
 
-  def initialize()
-    @player1 = nil
-    @player2 = nil
-    @board = nil
+  def initialize(player1 = nil, player2 = nil, board = nil)
+    @player1 = player1 || nil
+    @player2 = player2 || nil
+    @board = board || nil
   end
 
   def start_new_game
@@ -12,6 +13,27 @@ class Game
     puts "Enter the name of player 2, they will play as black"
     @player2 = Player.new(gets.chomp)
     @board = Board.new
+  end
+
+  def save_game
+    time = Date.now.to_s
+    Dir.mkdir("saves") unless Dir.exist? "saves"
+    save = to_yaml
+    filename = "saves/#{time}.txt"
+    File.open(filename, "w"){ |somefile| somefile.puts save}
+  end
+
+  def to_yaml
+    YAML.dump ({
+      :player1 => @player1,
+      :player2 => @player2,
+      :board => @board
+    })
+  end
+
+  def self.from_yaml(string)
+    data = YAML.load(string)
+    self.new(data[:player1], data[:player2], data[:board])
   end
 
 end
